7d54914ced92fc727b3d55cb9620f1f76b5af56d
Yahia Lakrikba <161433620+YahiaLakrikba2004@users.noreply.github.com>
2024-05-28 14:52:16 +0200

    Initial commit

---
+++ src/esercizio3/RubricaTelefonica.java
@@ -0,0 +1,54 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RubricaTelefonica {
+    private Map<String, String> rubrica;
+
+    public RubricaTelefonica() {
+        rubrica = new HashMap<>();
+    }
+
+    public void inserisciContatto(String nome, String numeroTelefono) {
+        rubrica.put(nome, numeroTelefono);
+    }
+
+    public void cancellaContatto(String nome) {
+        rubrica.remove(nome);
+    }
+
+    public String cercaNomeDaNumero(String numeroTelefono) {
+        for (Map.Entry<String, String> entry : rubrica.entrySet()) {
+            if (entry.getValue().equals(numeroTelefono)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public String cercaNumeroDaNome(String nome) {
+        return rubrica.get(nome);
+    }
+
+    public void stampaRubrica() {
+        for (Map.Entry<String, String> entry : rubrica.entrySet()) {
+            System.out.println("Nome: " + entry.getKey() + ", Telefono: " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        RubricaTelefonica rubrica = new RubricaTelefonica();
+        rubrica.inserisciContatto("Mario", "1234567890");
+        rubrica.inserisciContatto("Luigi", "0987654321");
+        rubrica.inserisciContatto("Peach", "1112223333");
+
+        System.out.println("Stampa dei contatti:");
+        rubrica.stampaRubrica();
+
+        System.out.println("\nNumero di telefono di Luigi: " + rubrica.cercaNumeroDaNome("Luigi"));
+        System.out.println("Nome associato al numero 1112223333: " + rubrica.cercaNomeDaNumero("1112223333"));
+
+        rubrica.cancellaContatto("Peach");
+        System.out.println("\nDopo la cancellazione di Peach:");
+        rubrica.stampaRubrica();
+    }
+}
